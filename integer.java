ae1fed0fe11c2916b103b7ad888cb04a5107e140
MONA-SAZZY <47383757+MONA-SAZZY@users.noreply.github.com>
2019-02-06 16:08:01 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class sum
+{
+    public static void main(String args[])
+    {
+        int num=2,sum=0;
+    for(int i=1;i<=num;++i)
+    {
+        sum+=i;
+    }
+   System.out.println("sum="+ sum);
+    
+}
+}
